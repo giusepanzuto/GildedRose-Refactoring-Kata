bc6a957772c5e3f9cf952400e145eec01dc3bf1a
Giuseppe Panzuto <giusepanzuto@gmail.com>
2022-10-23 11:42:32 +0200

    project structure; Item to abstrct class

---
+++ csharp/Items/Item.cs
@@ -0,0 +1,37 @@
+﻿namespace csharp
+{
+    public abstract class Item
+    {
+        public string Name { get; set; }
+        public int SellIn { get; set; }
+        public int Quality { get; set; }
+
+        public override string ToString()
+        {
+            return this.Name + ", " + this.SellIn + ", " + this.Quality;
+        }
+
+        public abstract void UpdateQuality();
+
+        protected void DecreaseQuality()
+        {
+            if (Quality > 0)
+            {
+                Quality = Quality - 1;
+            }
+        }
+
+        protected void ResetQuality()
+        {
+            Quality = 0;
+        }
+
+        protected void IncreaseQuality()
+        {
+            if (Quality < 50)
+            {
+                Quality = Quality + 1;
+            }
+        }
+    }
+}
